e8a50ce3347e07d9db814aa99b9f1b7e7b84782f
Akinyele-Samuel <akinwolemiwasamuel@gmail.com>
2024-03-12 15:31:14 +0000

    adafruit

--- dht_file/humidity.py
+++ dht_file/humidity.py
@@ -5,20 +5,43 @@
 #Based on Adafruit_CircuitPython_DHT
 
 #Libraries
+from sense_hat import SenseHat
+
 import sys
+
+# adapted from https://learn.adafruit.com/adafruit-io-basics-digital-output/python-code
+#import Adafruit IO REST client.
+from Adafruit_IO import Client, Feed, RequestError, Data
 import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
+
+# Set to your Adafruit IO key.
+# Remember, your key is a secret,
+# so make sure not to publish it when you publish this code!
+ADAFRUIT_IO_KEY = 'aio_BkVf53AQS6lpfYuoH2cArbQgDf8Z'
+
+# Set to your Adafruit IO username.
+# (go to https://accounts.adafruit.com to find your username)
+ADAFRUIT_IO_USERNAME = 'Akinyele_Samuel'
+sense = SenseHat()
+
+# Create an instance of the REST client.
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+TAG = "sensehat humidity publisher"
 
 from time import sleep
 
 
 while True:
         #Read Humidity from DHT22
-        humidity = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        humidity, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+        
         
         if humidity is not None:
-            print("Humidity: {0.1f}%".format(humidity))
+            #To print the humidity and format it to one decimal place
+            print("Humidity: {:.1f}%".format(humidity))
         
         else:
             print("Sensor failure, Please check wiring.")
